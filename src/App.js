3f9f4adb6c7b067179b671e4b3dea4f2ee89c120
NguyenDai2001 <dai242001@gmail.com>
2024-12-31 23:39:30 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.scss';
+
+const App = () => {
+  return (
+    <>
+      <h2 className='test'>aaaaaa</h2>
+      <p>aaa</p>
+      <button className='btn btn-primary'>sss</button>
+    </>
+  )
+}
+
+export default App;
